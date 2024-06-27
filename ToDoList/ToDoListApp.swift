7d9866c67ecb20eba8fdadf80351b1957314eff2
Meruem Moniker <jamesconnorhill@gmail.com>
2024-06-27 12:23:15 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Connor Hill on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
